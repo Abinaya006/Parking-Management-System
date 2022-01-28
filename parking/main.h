c1cbb41c139a67eb352ed8c86af77be7cff4b8d5
Abinaya006 <81465344+Abinaya006@users.noreply.github.com>
2022-01-28 18:55:29 +0530

    Add files via upload

---
+++ parking/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<stddef.h>
+#include<string.h>
+#include<windows.h>
+#include<time.h>
+
+#include "data.h"
+void login();
+void set_color(int sample);
+
+#endif
